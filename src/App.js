c193d3f124d79a12c0164c029ac38e70a5aba6d7
Sumit Lahiri <lahiri.devs@gmail.com>
2022-08-22 22:23:34 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div className="max-w-7xl mx-auto px-4 py-4 items-center sm:px-6 font-mono">
+      <div className="text-center text-white">
+
+      </div>
+      <div className="grid grid-rows-3 gap-2">
+        <div className="grid grid-cols-5 gap-2 border-2 border-indigo-400 shadow-lg rounded-xl px-10 py-5">
+          sumit
+        </div>
+        <div className="border-2 border-indigo-400 shadow-lg rounded-xl px-10 py-5">
+          sumit
+        </div>
+        <div className="border-2 border-indigo-400 shadow-lg rounded-xl px-10 py-5">
+          sumit
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
